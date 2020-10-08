0c37a35b6a155f74b3276574f5da1998201c6718
olafk <olaf.koziara@gmail.com>
2020-10-08 11:51:50 +0200

    Reedux devTools added

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,9 @@
 import { createStore } from "redux";
 import walletierReducer from "../reducers";
 
-const store = createStore(walletierReducer);
+const store = createStore(
+  walletierReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
 
 export default store;
